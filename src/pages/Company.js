2d7fdefd13a525715d2b5c49b11da8d25bdd98b5
YaswanthMallavarapu <mallavarapuyaswanth5@gmail.com>
2025-05-12 18:42:13 +0530

    helmet added

--- src/pages/Company.js
+++ src/pages/Company.js
@@ -4,9 +4,38 @@ import Community from "../components/Community";
 import MissionCards from "../components/Mission-features";
 import AboutUs from "../components/Ourmission-Aboutus";
 import OurMissionBottom from "../components/OurMission-Bottom";
+import { Helmet } from "react-helmet";
 const Company = () => {
   return (
     <div>
+      <Helmet>
+        <title>Careers at Kraf Technologies - Join Our Team</title>
+        <meta
+          name="description"
+          content="Join Kraf Technologies and be part of an innovative team. Explore exciting career opportunities in software development, AI, and cloud computing."
+        />
+        <meta
+          name="keywords"
+          content="Careers, Jobs, Software Development, AI, Cloud Computing, Kraf Technologies"
+        />
+        <meta
+          property="og:title"
+          content="Careers at Kraf Technologies - Join Our Team"
+        />
+        <meta
+          property="og:description"
+          content="Explore job openings and work with the best in the industry at Kraf Technologies."
+        />
+        <meta
+          property="og:image"
+          content="https://kraftechnologies.com/assets/careers-og-image.jpg"
+        />
+        <meta
+          property="og:url"
+          content="https://kraftechnologies.com/careers/"
+        />
+        <link rel="canonical" href="https://kraftechnologies.com/careers/" />
+      </Helmet>
       <OurMission />
       <Community />
       <MissionCards />
--- src/pages/NewServices.js
+++ src/pages/NewServices.js
@@ -4,10 +4,38 @@ import BusinessGrowthSection from "../components/BusinessGrowthSection";
 import AiMachineLearningSection from "../components/AiMachineLearningSection";
 import TechnologyStackSection from "../components/TechnologyStackSection";
 import InnovationTechnologySection from "../components/InnovationTechnologySection";
-
+import { Helmet } from "react-helmet";
 const NewServices = () => {
   return (
     <>
+      <Helmet>
+        <title>Careers at Kraf Technologies - Join Our Team</title>
+        <meta
+          name="description"
+          content="Join Kraf Technologies and be part of an innovative team. Explore exciting career opportunities in software development, AI, and cloud computing."
+        />
+        <meta
+          name="keywords"
+          content="Careers, Jobs, Software Development, AI, Cloud Computing, Kraf Technologies"
+        />
+        <meta
+          property="og:title"
+          content="Careers at Kraf Technologies - Join Our Team"
+        />
+        <meta
+          property="og:description"
+          content="Explore job openings and work with the best in the industry at Kraf Technologies."
+        />
+        <meta
+          property="og:image"
+          content="https://kraftechnologies.com/assets/careers-og-image.jpg"
+        />
+        <meta
+          property="og:url"
+          content="https://kraftechnologies.com/careers/"
+        />
+        <link rel="canonical" href="https://kraftechnologies.com/careers/" />
+      </Helmet>
       <OurServices />
       <BusinessGrowthSection />
       <AiMachineLearningSection />
